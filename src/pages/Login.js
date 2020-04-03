36451e2433e1df1e93b12b2da767337fa7ecbb90
jtx007 <jamesjacobthomas7@gmail.com>
2020-04-02 23:01:29 -0400

    added login button protection

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,20 +3,24 @@ import incub8API from '../api/incub8API'
 import { LoginContext } from '../context/loginContext'
 import { Redirect, navigate } from '@reach/router'
 import '../styles/form.css'
+import formHasEmptyFields from "../utils/form-has-empty-fields";
 
 const Login = (props) => {
 
-      const [values, setValues] = useState({ username: "", password: "" });
+      
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const formData = {
+      username,
+      password
+    }
 
-
-    const handleInputChange = e => {
-      const { name, value } = e.target;
-      setValues({ ...values, [name]: value.trim() });
-    };
+    
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await incub8API.post("/auth" , {"username": values.username, "password": values.password})
+        const response = await incub8API.post("/auth" , {"username": username, "password": password})
         console.log(response)
         localStorage.setItem("token", response.data.access_token)
         localStorage.setItem("user", response.data.username)
@@ -37,9 +41,9 @@ const Login = (props) => {
             <div className="uk-margin">
               <label className="uk-form-label">Username</label>
               <input
-                onChange={handleInputChange}
+                onChange={event => setUsername(event.target.value)}
                 name="username"
-                value={values.username}
+                value={username}
                 className="uk-input uk-form-width-large"
                 type="text"
               />
@@ -47,14 +51,15 @@ const Login = (props) => {
             <div className="uk-margin">
               <label className="uk-form-label">Password</label>
               <input
-                onChange={handleInputChange}
-                value={values.password}
+                onChange={event => setPassword(event.target.value)}
+                value={password}
                 name="password"
                 className="uk-input uk-form-width-large"
                 type="password"
               />
             </div>
             <button
+              disabled={formHasEmptyFields(formData)}
               type="submit"
               className="uk-button uk-button-default uk-text-capitalize form-submit-button"
             >
